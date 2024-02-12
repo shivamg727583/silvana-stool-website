782ca3ca8b1811f90987ba1b36015fd90c5a3a58
shivamg727583 <shivamg727583@gmail.com>
2024-02-12 19:58:00 +0530

    updated

--- script.js
+++ script.js
@@ -29,14 +29,46 @@ tl.from(".llink h4,.rlink span",{
 
 var tl2= gsap.timeline();
 
-tl.from(".sub-heading h3 , .right-heading p ,.btn",{
+tl2.from(".sub-heading h3,.right-heading p",{
     x:-300,
     opacity:0,
-    stagger:.4,
+    duration:1,
+    stagger:.3,
+    scrollTrigger:{
+        trigger:".sub-heading",
+        scroller:"body",
+        // markers:true,
+       start:"top 100%",
+       end:"top 0%",
+        scrub:2
+    }
+})
+tl2.from(".btn button,.btn i",{
+y:-200,
+opacity:0,
+duration:1,
+stagger:.3,
+scrollTrigger:{
+    trigger:".btn",
+    scroller:"body",
+    // markers:true,
+   start:"top 100%",
+   end:"top 0%",
+    scrub:5
+}
 })
 
-tl.from(".card,.card-nav,.card-heading,.card i",{
+
+tl2.from(".card,.card-nav,.card-heading,.card i",{
     y:200,
     opacity:0,
     stagger:.3,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 100%",
+        scrub:5
+    }
 })
